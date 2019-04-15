b9b948f0ab122c51c0047af6017f3fa231bc8d11
rrosztoczy <rrosztoczy@gmail.com>
2019-04-15 16:23:56 -0400

    updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # Monsters
-# Rubia = Monster.create(name: "Rubia", phrase: "I'm a new developers best friend!...")
-# NoSQL = Monster.create(name: "NoSQL", phrase: "WITH RECURSIVE q(r, i, rx, ix, g) AS (
-#     SELECT r::DOUBLE PRECISION * 0.02, i::DOUBLE PRECISION * 0.02, 
-#           .0::DOUBLE PRECISION      , .0::DOUBLE PRECISION, 0
-#     FROM generate_series(-60, 20) r, generate_series(-50, 50) i
-#     UNION ALL...")
-# Elixin = Monster.create(name: "Elixin", phrase: "I'm so fast and strong, catch me if you can!...")
-# Piethon = Monster.create(name: "Piethon", phrase: "Let's have pun with data!")
-# Javahscrypt = Monster.create(name: "Javahscrypt", phrase: "Jaaaaaaaaavvahscrypt")
-# Rustico = Monster.create(name: "Rustico", phrase: "?")
+rubia = Monster.create(name: "Rubia", phrase: "I'm a new developers best friend!...")
+noSQL = Monster.create(name: "NoSQL", phrase: "WITH RECURSIVE q(r, i, rx, ix, g) AS (
+    SELECT r::DOUBLE PRECISION * 0.02, i::DOUBLE PRECISION * 0.02, 
+          .0::DOUBLE PRECISION      , .0::DOUBLE PRECISION, 0
+    FROM generate_series(-60, 20) r, generate_series(-50, 50) i
+    UNION ALL...")
+elixin = Monster.create(name: "Elixin", phrase: "I'm so fast and strong, catch me if you can!...")
+piethon = Monster.create(name: "Piethon", phrase: "Let's have pun with data!")
+javahscrypt = Monster.create(name: "Javahscrypt", phrase: "Jaaaaaaaaavvahscrypt")
+rustico = Monster.create(name: "Rustico", phrase: "?")
 
 # Questions
 # Schema for new:
@@ -27,7 +27,7 @@
 # t.datetime "updated_at", null: false
 # t.index ["monster_id"], name: "index_questions_on_monster_id"
 # Rubia
-# rubyeasy = Question.create(question_text: "Which of these methods is mine?", difficulty: 1, monster_id: 16)
+rubyeasy = Question.create(question_text: "Which of these methods is mine?", difficulty: 1, monster_id: rubia.id)
 # rubymediun = Question.create(question_text: "How", difficulty: 1, monster_id: 1)
 # rubyhard = Question.create(question_text: "How", difficulty: 1, monster_id: 1)
 
@@ -41,10 +41,10 @@
 # t.datetime "created_at", null: false
 # t.datetime "updated_at", null: false
 # t.index ["question_id"], name: "index_answers_on_question_id"
-rubyeasya = Answer.create(answer_text: "Array.prototype.forEach()", letter: "A", correct: false, question_id: 2)
-rubyeasyb = Answer.create(answer_text: "Array.prototype.forEach()", letter: "B", correct: false, question_id: 2)
-rubyeasyc = Answer.create(answer_text: "Fireballs.each { |Fireball| puts `Spits fireball` }", letter: "C", correct: true, question_id: 2)
-rubyeasyd = Answer.create(answer_text: "Array.prototype.forEach()", letter: "D", correct: false, question_id: 2)
+rubyeasya = Answer.create(answer_text: "Array.prototype.forEach()", letter: "A", correct: false, question_id: rubyeasy.id)
+rubyeasyb = Answer.create(answer_text: "Array.prototype.forEach()", letter: "B", correct: false, question_id: rubyeasy.id)
+rubyeasyc = Answer.create(answer_text: "Fireballs.each { |Fireball| puts `Spits fireball` }", letter: "C", correct: true, question_id: rubyeasy.id)
+rubyeasyd = Answer.create(answer_text: "Array.prototype.forEach()", letter: "D", correct: false, question_id: rubyeasy.id)
 
 
 
