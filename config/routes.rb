37902d5bbb44332b20751661f046c376cdc4dae1
rrosztoczy <rrosztoczy@gmail.com>
2019-04-18 21:24:52 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   Rails.application.routes.draw do
+    root to: "api/v1/monsters#index"
     namespace :api do
       namespace :v1 do
         resources :monsters, only: [:index, :show]
